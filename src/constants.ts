2e2a6030512e819ed4f5f846c2ef40c4c2af2133
Dikiy Perec <122711062+Drygoy@users.noreply.github.com>
2024-12-16 00:16:38 +0600

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -11,7 +11,7 @@ export const SCROLL_LOCK_TIMEOUT = 100;
 
 export const BORDER_RADIUS = 8;
 
-export const NESTED_DISPLACEMENT = 16;
+export const NESTED_DISPLACEMENT = 48;
 
 export const WINDOW_TOP_OFFSET = 26;
 
